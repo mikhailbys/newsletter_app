d58c3ce37a5f01a3a8e6a38e7803294390995ebb
mikhailbys <bystrovmikhailad@gmail.com>
2020-10-09 23:32:16 +0400

    format

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import homeImgSource from '../../resources/forest.jpg'
 import useStyles from './styles'
 import {Footer} from "../Footer/Footer";
@@ -7,14 +7,13 @@ import {Footer} from "../Footer/Footer";
 export const Home = () => {
     const styles = useStyles();
     return (
-    <>
-        <div>
-            <img
-                className={styles.image}
-                src={homeImgSource}
-                alt="Forest"
-            />
-
+        <>
+            <div>
+                <img
+                    className={styles.image}
+                    src={homeImgSource}
+                    alt="Forest"
+                />
                 <section>
                     <Typography
                         variant="h3"
@@ -25,12 +24,16 @@ export const Home = () => {
                     <p
                         className={styles.p}
                     >
-                        JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. Read more about JavaScript.
+                        JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language
+                        with first-class functions. While it is most well-known as the scripting language for Web pages,
+                        many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat.
+                        JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting
+                        object-oriented, imperative, and declarative (e.g. functional programming) styles. Read more
+                        about JavaScript.
                     </p>
                 </section>
-
-        </div>
-        <Footer/>
-    </>
+            </div>
+            <Footer/>
+        </>
     );
 }
